17bf6d82a17a5e1c5709683f5bbac40720ee211b
Hao Yun <Haoy2001@gmail.com>
2019-06-22 21:57:40 +0800

    UI started

---
+++ Assets/Scenes/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int enemyHealth;
+    public int playerHealth;
+
+    void reset(){
+        enemyHealth = 200;
+        playerHealth = 200;
+    }
+
+    void Start()
+    {
+        reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
