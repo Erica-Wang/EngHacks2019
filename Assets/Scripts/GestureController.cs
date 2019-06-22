11b82803f6478c84e09d53621132e0495c475b48
Erica <erica.wang.moyang@gmail.com>
2019-06-22 09:25:59 -0400

    try gesture

--- Assets/Scripts/GestureController.cs
+++ Assets/Scripts/GestureController.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Leap;
 
 public class GestureController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    Controller controller;
+
     void Start()
     {
-        
+        controller = new Controller();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        Frame frame = controller.Frame();
         
     }
 }
